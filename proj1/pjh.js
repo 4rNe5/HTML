d4a0c3df69c287f61a3eb01e246c94fc45a65413
박준현 <23wdwad@gmail.com>
2023-06-14 14:22:49 +0900

    Initial commit

---
+++ proj1/pjh.js
@@ -0,0 +1,7 @@
+function do_button_click(pwork){
+    if(pwork=='contents')document.getElementById("wpahr").innerHTML="내가 박준현이다!";
+    else if(pwork=='attr')document.getElementById("qnwpahr").style="color: green";
+    else if(pwork=='change_img')document.getElementById("imgsss").src="http://arabbit.link/images/tiger.png";
+    else if(pwork=="tnarlrl")document.getElementById("imgsss").style="display:none";
+    else if(pwork=="qhdlrl")document.getElementById("imgsss").style="display=block";
+}
